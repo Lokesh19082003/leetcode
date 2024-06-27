4a7ae0fcb55424c753505cb34c9475fb69b87ae5
LOKESH A <166640652+Lokesh19082003@users.noreply.github.com>
2024-06-27 18:30:47 +0530

    Time: 0 ms (100%), Space: 45.2 MB (95.69%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n= s.length;
+        int low=0,high=n-1;
+        while(low<=high){
+            char ch=s[low];
+            s[low]=s[high];
+            s[high]=ch;
+            low++;
+            high--;
+        }
+    }
+}
