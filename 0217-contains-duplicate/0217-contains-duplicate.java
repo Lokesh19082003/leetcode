274b7c5385bc3443e2634d09d3268fc90b80af93
LOKESH A <166640652+Lokesh19082003@users.noreply.github.com>
2024-07-04 16:32:22 +0530

    Time: 11 ms (70.89%), Space: 58.3 MB (47.97%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+         Set<Integer> seenNumbers = new HashSet<>();
+        for (int num : nums) {
+            if (seenNumbers.contains(num)) {
+                return true;
+            }
+            seenNumbers.add(num);
+        }
+        return false;
+    }
+}
